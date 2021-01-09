e5a9e5fc1cf596a67565cab1ead26071e1890244
Bumblebee Mountain <bumblebeemountain@gmail.com>
2021-01-09 22:23:38 +0000

    Re-arange pressureLog structure so that it script runs through cron

--- pressureLog.py
+++ pressureLog.py
@@ -8,29 +8,45 @@ import time
 import datetime
 
 sense = SenseHat()
+global MAX_TRY
+MAX_TRY = 4
 
 def now():
     return int(time.time())
 
+def getPressure():
+    return sense.get_pressure()
+
+def takeRecord(apiKey, loc, dataFile):
+    p = 0
+    tries = 0
+    while p==0:
+        p = getPressure()
+        tries += 1
+
+        if tries > MAX_TRY:
+            raise Exception("MAX TRIES REACHED FOR PRESSURE READING")
+
+        time.sleep(2)
+
+    temp = wth.getTemp(apiKey, loc)
+
+    dataFile.write("{}, {:.1f}, {}\n".format(now(), p, temp))
+    dataFile.flush()
+
 def main(dataFile):
     try:
-        apiKey = wth.getApiKey()
-        loc = wth.getLocation()
+        apiKey = wth.getApiKey("/home/pi/weather-log/apiKey.secret")
+        loc = wth.getLocation("/home/pi/weather-log/location.secret")
 
-        while True:
-            p = sense.get_pressure()
-            temperature = wth.getTemp(apiKey, loc)
+        takeRecord(apiKey, loc, dataFile)
 
-            dataFile.write("{}, {:.1f}, {}\n".format(now(), p, temperature))
-            dataFile.flush()
-            time.sleep(1800)
     except Exception as e:
         print("something went wrong:\n{}".format(e))
         sense.clear()
     finally:
-        print("closing")
         dataFile.close()
 
 if __name__ == '__main__':
-    data = open("pressure.csv", "a+")
+    data = open("/home/pi/weather-log/pressure.csv", "a+")
     main(data)
